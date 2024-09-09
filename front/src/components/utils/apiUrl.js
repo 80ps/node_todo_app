67e8fc361baf0fbd6263378ae1284bfb01fb8b61
80sp <yjsion8085@gmail.com>
2024-09-09 10:42:41 +0900

    first commit

---
+++ front/src/components/utils/apiUrl.js
@@ -0,0 +1,14 @@
+const GET_TASKS_API_URL = 'http://localhost:3000/get_tasks';
+const POST_TASKS_API_URL = 'http://localhost:3000/post_task';
+const DELETE_TASKS_API_URL = 'http://localhost:3000/delete_task';
+const UPDATE_COMPLETED_TASKS_API_URL =
+  'http://localhost:8080/update_completed_task';
+const UPDATE_TASK_API_URL = 'http://localhost:3000/update_task';
+
+export {
+  GET_TASKS_API_URL,
+  POST_TASKS_API_URL,
+  DELETE_TASKS_API_URL,
+  UPDATE_COMPLETED_TASKS_API_URL,
+  UPDATE_TASK_API_URL,
+};
